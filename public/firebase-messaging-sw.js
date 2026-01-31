83b8a0f864b22e46208c3dd4cdd8b8c579b8cbb5
joyceobro <seliscos@gmail.com>
2026-02-01 01:14:23 +0900

    swjs33 20260201

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // public/sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-sw.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-sw.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // 앱과 동일한 설정값 (반드시 동일해야 함!)
 firebase.initializeApp({
